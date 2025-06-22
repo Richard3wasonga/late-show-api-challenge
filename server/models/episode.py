6403d5fc9fa953443b00a08703f18329a322478f
Richard Wasonga <rowwasonga@gmail.com>
2025-06-22 11:39:36 +0300

    corrected typo in episodes.py

--- server/models/episode.py
+++ server/models/episode.py
@@ -5,7 +5,7 @@ class Episode(db.Model, SerializerMixin):
     __tablename__= "episodes"
 
     id = db.Column(db.Integer, primary_key=True)
-    data = db.Column(db.Date, nullable=False)
+    date = db.Column(db.Date, nullable=False)
     number = db.Column(db.Integer, nullable=False)
     appearances = db.relationship("Appearance", back_populates="episode", cascade="all, delete-orphan")
     guests = association_proxy("appearances", "guest" creator=lambda guest: Appearance(guest=guest))
