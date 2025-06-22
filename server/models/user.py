49b15649f4c71ae18cb081e507ae2343cddc4f59
Richard Wasonga <rowwasonga@gmail.com>
2025-06-22 16:44:57 +0300

    edited user model password title to passsword_hash

--- server/models/user.py
+++ server/models/user.py
@@ -8,13 +8,13 @@ class User(db.Model, SerializerMixin):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True, nullable=False)
-    password = db.Column(db.String, nullable=False)
+    password_hash = db.Column(db.String, nullable=False)
 
     def set_password(self, password):
-        self.password = generate_password_hash(password)
+        self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
-        return check_password_hash(self.password, password)
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
-        return f"<user id={self.id} username={self.username}>" 
+        return f"<User id={self.id} username={self.username}>" 
