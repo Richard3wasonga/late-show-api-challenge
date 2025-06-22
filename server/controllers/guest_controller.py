38e6ff75cf6bcc919ad2d922e050c059b5593d93
Richard Wasonga <rowwasonga@gmail.com>
2025-06-22 14:33:38 +0300

    added route to guest_controller

---
+++ server/controllers/guest_controller.py
@@ -0,0 +1,17 @@
+from flask import Blueprint, jsonify, make_response
+from flask_restful import Api, Resource
+from ..models import Guest, db
+
+guest_bp = Blueprint('guest', __name__)
+api = Api(guest_bp)
+
+class GuestList(Resource):
+    def get(self):
+        guests = db.session.query(Guest).all()
+        if not guests:
+            return make_response(jsonify({'message': 'No guests found', 'guests': []}), 200)
+
+        guest_list = [guest.to_dict() for guest in guests]
+        return make_response(jsonify(guest_list), 200)
+
+api.add_resource(GuestList, '/guests')
